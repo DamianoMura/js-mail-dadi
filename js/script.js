1d4d2eb0726883be7234ba34fb3927fd308a827e
DamianoMura <mura.damiano14@gmail.com>
2025-07-05 14:58:57 +0200

    [add] main structure + (mail) mail list variable

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// iniziamo con le mail, creando un array contenente le mail di partenza
+// e poi tramite prompt facciamo inserire all'utente la propria mail
+//per poi andarla a cercare nell'array e stampare se si ha accesso o meno 
+
+const mails= [
+"nico@gmail.com",
+"pagassasabe@tiscali.com",
+"pasaghella@yahoo.com",
+"simpo@gmail.com",
+"gagnella@gmail.com",
+"franco@gmail.com",
+"cassacarragnu@gmail.com",
+//ogni riferimento a nomi o persone è puramente casuale
+];
+
