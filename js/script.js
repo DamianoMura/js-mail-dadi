f2ddcb9cba93a7fca14d85ed68c0a6d58d72bd7a
DamianoMura <mura.damiano14@gmail.com>
2025-07-05 15:02:22 +0200

    [add]  (mail) mail list variables + considerations

--- js/script.js
+++ js/script.js
@@ -13,3 +13,7 @@ const mails= [
 //ogni riferimento a nomi o persone è puramente casuale
 ];
 
+// chiediamo all'utente di inserire la propria e-mail e dichiariamo anche la variabile userMail dove immagazzineremo l'input
+const userMail = prompt("inserisci la tua mail");
+
+//utilizziamo il for per andare a confrontare l'input con ogni posizione dell'array mails , e in caso sia uguale (condizione per poter accedere) darò il permesso e in caso contrario no
